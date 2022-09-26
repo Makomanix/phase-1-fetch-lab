2e7747eee7b012a31cf937e347fea456875bf41c
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-25 22:01:57 -0400

    commit tree bdf7f5d1f8d2b0f22f5c9b5f56637007c2dec054

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json));
+  return fetch('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(books) {
@@ -15,3 +18,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
